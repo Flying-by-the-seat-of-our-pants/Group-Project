ae28ee1aa4c6bc9472db74ec43314d1fa2f4903a
Dave <davidmcdonald40@gmail.com>
2021-03-21 20:20:18 -0600

    added c-tor to User class

--- Task-Manager-Project/app/src/main/java/Task/Manager/Project/User.java
+++ Task-Manager-Project/app/src/main/java/Task/Manager/Project/User.java
@@ -11,7 +11,76 @@ public class User {
     private Date registerDate;
     private int userID; // unique ID for every user?
 
+    /***********************
+     * Constructors
+     * *********************/
+    public User(String email, String userName, String password,
+                String loginStatus, Date registerDate, int userID) {
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+        this.loginStatus = loginStatus;
+        this.registerDate = registerDate;
+        this.userID = userID;
+    }
 
+    public User(String email, String userName, String password) {
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
 
+    /***********************
+     * Getters
+     * *********************/
+    public String getEmail() {
+        return email;
+    }
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    /***********************
+     * Setters
+     * *********************/
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
 }
